d92f3ca18efbe99d51cd1fb8870684796d1a59ec
Ehqht <livia3609@gmail.com>
2018-08-15 14:44:50 +0900

    FPS (압력 센서) -click control
    우클릭 좌클릭 구분 가능
    회로 저항에 따라 기준값 다르게
    -본 코드는 저항 10k 일때

---
+++ FSR_click_standard/FSR_click_standard.ino
@@ -0,0 +1,30 @@
+//1키로 옴 저항(갈검검) - 기준값 180
+#include <Mouse.h>
+
+int RLED = 8;
+int GLED = 3;
+void setup() {
+  pinMode(RLED, OUTPUT);
+  pinMode(GLED, OUTPUT);
+  Serial.begin(9600);
+  Mouse.begin();
+}
+void loop() {
+  int sensor_L = analogRead(A0);
+  int sensor_R = analogRead(A1);
+  if(sensor_L>180){
+  digitalWrite(RLED,HIGH);
+  Mouse.click(); 
+  }
+  if(sensor_R>180){
+    digitalWrite(GLED,HIGH);
+    Mouse.click(MOUSE_RIGHT);
+  }
+  else{
+    digitalWrite(RLED,LOW);
+    digitalWrite(GLED,LOW);
+  }
+  delay(100);
+ Serial .println("Left Sensor : " + String(sensor_L) + "  Right Sensor : " +String(sensor_R) );
+  delay(100);
+}
