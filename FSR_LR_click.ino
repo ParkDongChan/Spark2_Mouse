a5391178d8eb60992147ce8523ed77a1809acc40
Ehqht <livia3609@gmail.com>
2018-08-16 14:31:48 +0900

    FSR Left and Right Click
    LED X
    Serial monitor O

---
+++ FSR_LR_click.ino
@@ -0,0 +1,21 @@
+//1키로 옴 저항(갈검검) - 기준값 180
+#include <Mouse.h>
+
+
+void setup() {
+  Serial.begin(9600);
+  Mouse.begin();
+}
+void loop() {
+  int sensor_L = analogRead(A0);
+  int sensor_R = analogRead(A1);
+  if(sensor_L>180){
+  Mouse.click(); 
+  }
+  if(sensor_R>180){
+    Mouse.click(MOUSE_RIGHT);
+  }
+  delay(100);
+ Serial .println("Left Sensor : " + String(sensor_L) + "  Right Sensor : " +String(sensor_R) );
+  delay(100);
+}
