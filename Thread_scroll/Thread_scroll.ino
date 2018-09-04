5880f948ab69ce71ab44aa1a012f4868db0d398f
Ehqht <livia3609@gmail.com>
2018-09-04 14:44:49 +0900

    Thread scroll
    스레드에서 스크롤 구현

---
+++ Thread_scroll/Thread_scroll.ino
@@ -0,0 +1,146 @@
+#include <Thread.h>
+#include <ThreadController.h>
+#include <Wire.h>
+#include <Mouse.h>
+
+long accelX, accelY, accelZ;
+float gForceX, gForceY, gForceZ;
+long gyroX, gyroY, gyroZ;
+float rotX, rotY, rotZ;
+int sensor_L = analogRead(A0);
+int sensor_R = analogRead(A1);
+
+// ThreadController that will controll all threads
+ThreadController controll = ThreadController();
+
+
+Thread myThread = Thread();
+Thread hisThread = Thread();
+
+// callback for myThread
+void click(){
+  int sensor_L = analogRead(A0); 
+  int sensor_R = analogRead(A1);
+  Serial .println("Left Sensor : " + String(sensor_L) );
+  Serial .println("Right Sensor : " + String(sensor_R) );
+  if(sensor_L>=150 && sensor_R<150) {
+    Mouse.press(); 
+  }
+  if(sensor_R>=150 && sensor_L<150) {
+    Mouse.click(MOUSE_RIGHT);
+  }
+  else if (sensor_L<150) {
+    Mouse.release();
+  }
+}
+
+// callback for hisThread
+void cursor(){
+  recordAccelRegisters();
+  recordGyroRegisters();
+  presssensorRegisters();
+  printData();
+}
+
+void setup(){
+  // Configure myThread
+  myThread.onRun(click);
+  myThread.setInterval(200);
+
+  // Configure myThread
+  hisThread.onRun(cursor);
+  hisThread.setInterval(10);
+
+  // Adds both threads to the controller
+  controll.add(&myThread);
+  controll.add(&hisThread); // & to pass the pointer to it
+  
+  Serial.begin(9600);
+  Wire.begin();
+  setupMPU();
+  Mouse.begin();
+}
+
+void loop(){
+  // run ThreadController
+  // this will check every thread inside ThreadController,
+  // if it should run. If yes, he will run it;
+  controll.run();
+
+  // Rest of code
+  float h = 3.1415;
+  h/=2;
+}
+
+void setupMPU(){
+  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
+  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
+  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
+  Wire.endTransmission();  
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
+  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s 
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
+  Wire.write(0b00000000); //Setting the accel to +/- 2g
+  Wire.endTransmission(); 
+}
+
+void recordAccelRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x3B); //Starting register for Accel Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000,6); //Request Accel Registers (3B - 40)
+  while(Wire.available() < 6);
+  accelX = Wire.read()<<8|Wire.read(); //Store first two bytes into accelX
+  accelY = Wire.read()<<8|Wire.read(); //Store middle two bytes into accelY
+  accelZ = Wire.read()<<8|Wire.read(); //Store last two bytes into accelZ
+  processAccelData();
+}
+
+void presssensorRegisters() {
+  sensor_L = analogRead(A0); 
+  sensor_R = analogRead(A1);
+}
+
+void processAccelData(){
+  gForceX = accelX / 20000.0;
+  gForceY = accelY / 20000.0; 
+  gForceZ = accelZ / 20000.0;
+}
+
+void recordGyroRegisters() {
+  Wire.beginTransmission(0b1101000); //I2C address of the MPU
+  Wire.write(0x43); //Starting register for Gyro Readings
+  Wire.endTransmission();
+  Wire.requestFrom(0b1101000,6); //Request Gyro Registers (43 - 48)
+  while(Wire.available() < 6);
+  gyroX = Wire.read()<<8|Wire.read(); //Store first two bytes into accelX
+  gyroY = Wire.read()<<8|Wire.read(); //Store middle two bytes into accelY
+  gyroZ = Wire.read()<<8|Wire.read(); //Store last two bytes into accelZ
+  processGyroData();
+}
+
+void processGyroData() {
+  rotX = gyroX / 131.0;
+  rotY = gyroY / 131.0;
+  rotZ = gyroZ / 131.0;
+}
+
+void printData() {  
+  if((gForceX>0.15 || gForceX < -0.15) || (gForceY>0.15 || gForceY < -0.15)){
+      gForceX*=-20;
+      gForceY*=20;
+      Mouse.move(gForceX,gForceY);
+  }
+  if(sensor_L>=150 && sensor_R>=150) {
+    if(gForceX>0) {
+      Mouse.move(0,0,2);
+    }
+    if(gForceX<0) {
+      Mouse.move(0,0,-2);
+    }
+  }
+}
+  
